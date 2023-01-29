999ba1baa1d5681f236f4aa47f9235f0dc526060
adamdgit <adam_d@outlook.com>
2023-01-29 13:16:50 +1100

    Removed svg animations

--- js/scrollAnimations.js
+++ js/scrollAnimations.js
@@ -1,10 +1,5 @@
 const bttButton = document.querySelector('.btt-bubble')
 const bttLink = document.querySelector('.btt-arrow')
-const svgs = document.querySelectorAll('.left svg')
-const projectsSection = document.querySelector('.projects')
-
-let projectsOffset = projectsSection.offsetHeight
-let projectsHeight = projectsSection.clientHeight
 
 window.addEventListener('scroll', () => {
 
@@ -22,19 +17,6 @@ window.addEventListener('scroll', () => {
     bttButton.style.transform = 'rotate(0deg)'
     bttLink.setAttribute('href', '#')
     bttLink.setAttribute('title', 'Back to top')
-  }
-  // svgs
-  if (window.innerHeight < 900) {
-    return;
-  } else {
-    if(scroll > (projectsOffset + window.innerHeight / 3) 
-    && scroll < projectsOffset + projectsHeight * 1.2) {
-      svgs[0].style.transform = `translateY(${scroll / 10 -250}px)`
-      svgs[1].style.transform = `translateY(-${scroll / 30 -60}px)`
-      svgs[2].style.transform = `translateY(${scroll / 30 -50}px)`
-      svgs[3].style.transform = `translateY(-${scroll / 20 -100}px)`
-      svgs[4].style.transform = `translateY(${scroll / 20 -150}px)`
-    }
   }
 
 })
