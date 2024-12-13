7f28d6a9df5c67707b853d49f2cab8ce3a4433f3
adamdgit <adam_d@outlook.com>
2024-12-13 17:02:45 +1100

    minor style improvements

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const mobile_menu_btn = document.querySelector('.mobile-dropdown-toggle');
 const navbar = document.querySelector('.navbar');
 
-mobile_menu_btn.addEventListener('click', () => {
+mobile_menu_btn.addEventListener('pointerdown', () => {
   navbar.classList.toggle("show");
 });
